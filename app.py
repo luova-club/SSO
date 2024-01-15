9ef6e3be723b18eb48256ae1dc03028b4d1b8db8
Verso <vuoreol@pm.me>
2024-01-15 14:00:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, jsonify, request, session, abort
-from flask_restful import Api, Resource
+from flask import Flask, jsonify, request, session
+from flask_restful import Api, Resource, reqparse
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from flask_pymongo import PyMongo
 import hashlib
 import secrets
+import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -23,6 +24,13 @@ class User(db.Model):
     password = db.Column(db.String(64), nullable=False)
     data = db.Column(db.String(255), nullable=False)
 
+# Define the Token model for storing tokens
+class Token(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    token = db.Column(db.String(32), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
 # DataManager class for database abstraction
 class DataManager:
     def __init__(self, app):
@@ -32,6 +40,12 @@ class DataManager:
         raise NotImplementedError("Method not implemented")
 
     def get_user(self, username, password):
+        raise NotImplementedError("Method not implemented")
+
+    def add_token(self, username, token):
+        raise NotImplementedError("Method not implemented")
+
+    def get_token(self, token):
         raise NotImplementedError("Method not implemented")
 
 # SQLDataManager for SQL databases
@@ -44,6 +58,14 @@ class SQLDataManager(DataManager):
     def get_user(self, username, password):
         return User.query.filter_by(username=username, password=hashlib.sha256(password.encode()).hexdigest()).first()
 
+    def add_token(self, username, token):
+        new_token = Token(username=username, token=token)
+        db.session.add(new_token)
+        db.session.commit()
+
+    def get_token(self, token):
+        return Token.query.filter_by(token=token).first()
+
 # MongoDataManager for MongoDB
 class MongoDataManager(DataManager):
     def add_user(self, username, password, data):
@@ -51,6 +73,12 @@ class MongoDataManager(DataManager):
 
     def get_user(self, username, password):
         return mongo.db.users.find_one({'username': username, 'password': hashlib.sha256(password.encode()).hexdigest()})
+
+    def add_token(self, username, token):
+        mongo.db.tokens.insert_one({'username': username, 'token': token})
+
+    def get_token(self, token):
+        return mongo.db.tokens.find_one({'token': token})
 
 # TokenManager for token verification
 class TokenManager:
@@ -68,8 +96,8 @@ class HomeResource(Resource):
         if 'username' in session:
             username = session['username']
             user_data = data_manager.get_user(username, session['password'])
-            return f'Welcome, {username}! <br> Your Data: {user_data["data"]}'
-        return 'You are not logged in.'
+            return {'message': f'Welcome, {username}!', 'data': user_data["data"]}
+        return {'error': 'You are not logged in.'}, 401
 
 class LoginResource(Resource):
     def post(self):
@@ -83,12 +111,19 @@ class LoginResource(Resource):
             session['username'] = username
             session['token'] = token
             session['password'] = password
+
+            # Save the token to the database
+            data_manager.add_token(username, token)
+
             return {'token': token}
         else:
             return {'error': 'Invalid username or password'}, 401
 
 class LogoutResource(Resource):
     def get(self):
+        # Remove the token from the database
+        if 'token' in session:
+            data_manager.get_token(session['token']).delete()
         session.clear()
         return {'message': 'Logged out successfully'}
 
@@ -101,6 +136,22 @@ class TokenVerificationResource(Resource):
         else:
             return {'error': 'Invalid token'}, 401
 
+class UserDataUpdateResource(Resource):
+    def put(self):
+        if 'username' in session:
+            username = session['username']
+            # Implement data update logic here
+            return {'message': 'User data updated successfully'}
+        return {'error': 'You are not logged in.'}, 401
+
+class UserDataDeletionResource(Resource):
+    def delete(self):
+        if 'username' in session:
+            username = session['username']
+            # Implement data deletion logic here
+            return {'message': 'User data deleted successfully'}
+        return {'error': 'You are not logged in.'}, 401
+
 # Choose the DataManager and TokenManager based on your desired database and token verification method
 data_manager = MongoDataManager(app)
 token_manager = SimpleTokenManager()
@@ -109,6 +160,8 @@ api.add_resource(HomeResource, '/')
 api.add_resource(LoginResource, '/login')
 api.add_resource(LogoutResource, '/logout')
 api.add_resource(TokenVerificationResource, '/verify-token')
+api.add_resource(UserDataUpdateResource, '/update-data')
+api.add_resource(UserDataDeletionResource, '/delete-data')
 
 if __name__ == '__main__':
     db.create_all()
